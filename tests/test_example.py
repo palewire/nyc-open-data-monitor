28e06d58481f7586e09bd7d3a58ec37b75c94615
palewire <b@palewi.re>
2023-11-13 05:52:40 -0500

    trim

--- tests/test_example.py
+++
@@ -1,7 +0,0 @@
-"""Test the module."""
-from src.download import get_url
-
-
-def test_get_url():
-    """Test the get_url method."""
-    get_url()
---
+++ tests/test_module.py
@@ -0,0 +1,13 @@
+"""Test the module."""
+from src.download import get_url
+from src.utils import get_sorted_file_list
+
+
+def test_get_url():
+    """Test the get_url method."""
+    get_url()
+
+
+def test_get_sorted_file_list():
+    """Get a sorted list of files."""
+    get_sorted_file_list()
