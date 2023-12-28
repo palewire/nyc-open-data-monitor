6e45a7b7aabc346003573bf04d841f5739510c91
Ben Welsh <b@palewi.re>
2023-12-28 13:27:20 +0000

    More logging for RSS command

--- src/rss.py
+++ src/rss.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import click
 import pandas as pd
+from rich import print
 from feedgen.feed import FeedGenerator
 
 from . import utils
@@ -18,14 +19,21 @@ from . import utils
 )
 def rss(verbose: bool) -> None:
     """Create an RSS feed with the most recently created datasets."""
+    if verbose:
+        print("Creating an RSS feed with the most recently created datasets.")
+
     clean_dir = utils.DATA_DIR / "clean"
     # Get the latest data
     df = pd.read_csv(
         clean_dir / "microdata.csv",
         parse_dates=["scrape_date", "creation_date", "update_date"],
     )
+    if verbose:
+        print(f"Loaded {len(df)} rows of data.")
 
     # Sort by created date and list the 20 most recent
+    if verbose:
+        print("Sorting by creation date and listing the 20 most recent.")
     df = df.sort_values("creation_date", ascending=False).head(20)
 
     # Convert into a list of dictionaries
@@ -40,6 +48,9 @@ def rss(verbose: bool) -> None:
     # Add the items
     for item in item_list:
         entry = feed.add_entry(order="append")
+        if verbose:
+            print(f"Adding {item['id']} to the RSS feed with the following metadata:")
+            print(item)
         entry.id(item["id"])
         entry.title(item["name"])
         entry.link(href=item["permalink"])
@@ -50,6 +61,8 @@ def rss(verbose: bool) -> None:
             entry.category(term=item["category"])
 
     # Write the feed
+    if verbose:
+        print(f"Writing the RSS feed to {clean_dir / 'latest-created.rss'}")
     feed.rss_file(clean_dir / "latest-created.rss")
 
 
